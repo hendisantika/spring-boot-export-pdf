6743b4af25f4a7f3399d0be1f03069c959ecb4e8
hendisantika <hendisantika@yahoo.co.id>
2024-04-16 08:38:44 +0700

    Add init data🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springbootexportpdf/SpringBootExportPdfApplication.java
+++ src/main/java/id/my/hendisantika/springbootexportpdf/SpringBootExportPdfApplication.java
@@ -1,7 +1,11 @@
 package id.my.hendisantika.springbootexportpdf;
 
+import id.my.hendisantika.springbootexportpdf.entity.Student;
+import id.my.hendisantika.springbootexportpdf.service.StudentService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringBootExportPdfApplication {
@@ -10,4 +14,16 @@ public class SpringBootExportPdfApplication {
         SpringApplication.run(SpringBootExportPdfApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner initData(StudentService studentService) {
+        return args -> {
+            for (int i = 0; i <= 10; i++) {
+                Student student = new Student();
+                student.setStudentName("Student Name " + i);
+                student.setEmail("student" + i + "@yopmail.com");
+                student.setMobileNo("+62813214118" + i);
+                studentService.addStudent(student);
+            }
+        };
+    }
 }
