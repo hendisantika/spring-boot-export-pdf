032d0343e57a52e87f54aa1edae88413204e68cd
hendisantika <hendisantika@yahoo.co.id>
2024-04-16 08:29:23 +0700

    Create starter project 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootexportpdf/SpringBootExportPdfApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.springbootexportpdf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootExportPdfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootExportPdfApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/springbootexportpdf/SpringBootExportPdfApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.springbootexportpdf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootExportPdfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
