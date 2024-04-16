e005f5ba0a9f72dd997eb47af23a3176c781a65a
hendisantika <hendisantika@yahoo.co.id>
2024-04-16 08:31:53 +0700

    Add Student Entity🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootexportpdf/entity/Student.java
@@ -0,0 +1,32 @@
+package id.my.hendisantika.springbootexportpdf.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-export-pdf
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 4/16/24
+ * Time: 08:31
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String studentName;
+    private String email;
+    private String mobileNo;
+}
